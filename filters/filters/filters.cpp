22fdc02d2821cc42e547a76a2aebce5c436a528a
Niklas Johansson <rapsabamse1@gmail.com>
2023-10-13 11:47:23 +0100

    Threshold par

--- filters/filters/filters.cpp
+++ filters/filters/filters.cpp
@@ -282,10 +282,10 @@ void *threadUpdateImg(void * thread_arg){
 
     //Start at thread id
     //Jump the amount of threads
-     sum = *my_data->sum;
+    unsigned sum = *my_data->sum;
     unsigned psum {};
     for (auto i { my_data->thread_id }; i < my_data->nump; i += my_data->thread_amount) {
-        psum = dstR[i] + dstG[i] + dstB[i];
+        psum = my_data->dstR[i] + my_data->dstG[i] + my_data->dstB[i];
         if (sum > psum) {
            my_data->dstR[i] = my_data->dstG[i] = my_data->dstB[i] = 0;
         } else {
