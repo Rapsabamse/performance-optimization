129b155aece6db009d9bb94df8c5dd97d25ad03f
Niklas Johansson <rapsabamse1@gmail.com>
2023-10-13 12:00:22 +0100

    Threshold par

--- filters/filters/filters.cpp
+++ filters/filters/filters.cpp
@@ -284,7 +284,6 @@ void *threadUpdateImg(void * thread_arg){
     //Jump the amount of threads
     unsigned sum = *my_data->sum;
     unsigned psum {};
-    //std::cout << "sum: " << sum << " nump: " << my_data->nump << "\n";
     for (auto i { my_data->thread_id }; i < my_data->nump; i += my_data->thread_amount) {
         psum = my_data->dstR[i] + my_data->dstG[i] + my_data->dstB[i];
         if (sum > psum) {
@@ -340,8 +339,6 @@ Matrix threshold_par(Matrix &m, const int MAX_THREADS)
 
     sum /= nump;
 
-    unsigned psum {};
-    std::cout << "realsum: " << sum << " real nump = " << nump << "\n\n";
     for(int i= 0; i < MAX_THREADS; i++){
         thread_data_array[i].thread_id = i;
         thread_data_array[i].thread_amount = MAX_THREADS;
