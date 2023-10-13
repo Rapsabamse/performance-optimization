f7b377a1e79b613b8cafb8dee533834babb831e9
Niklas Johansson <rapsabamse1@gmail.com>
2023-10-13 14:29:10 +0100

    blur

--- filters/filters/filters.cpp
+++ filters/filters/filters.cpp
@@ -194,7 +194,7 @@ void *threadblurX(void * thread_arg){
     int x_loop = 0;
     int y_loop = 0;
     int wi_loop = 0;
-    auto i { my_data->thread_id }; i < my_data->nump; i += my_data->thread_amount
+    //auto i { my_data->thread_id }; i < my_data->nump; i += my_data->thread_amount;
     for (auto x { my_data->thread_id }; x < dstXsize; x += my_data->thread_amount) {
         for (auto y { my_data->thread_id }; y < dstYsize; y += my_data->thread_amount) {
             //auto r { my_data->w[0] * dst.r(x, y) }, g { my_data->w[0] * dst.g(x, y) }, b { my_data->w[0] * dst.b(x, y) }, n { my_data->w[0] };
