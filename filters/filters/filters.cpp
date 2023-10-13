d1b37a3a404402a3b30a890e7631262626dc8ec2
Niklas Johansson <rapsabamse1@gmail.com>
2023-10-13 14:14:28 +0100

    blur

--- filters/filters/filters.cpp
+++ filters/filters/filters.cpp
@@ -190,8 +190,10 @@ void *threadblurX(void * thread_arg){
     std::cout << " dstXSize: " << dstXsize << " dstYsize: " << dstYsize;
     std::cout << " W: " << w[0] <<"\n\n";
 
-    for (auto x { my_data->thread_id }; x < dstXsize; x += my_data->thread_amount) {
-        for (auto y { my_data->thread_id }; y < dstYsize; y += my_data->thread_amount) {
+    //for (auto x { my_data->thread_id }; x < dstXsize; x += my_data->thread_amount) {
+    //    for (auto y { my_data->thread_id }; y < dstYsize; y += my_data->thread_amount) {
+    for (auto x { 0 }; x < dstXsize; x++) {
+        for (auto y { 0 }; y < dstYSize; y++) {
             //auto r { my_data->w[0] * dst.r(x, y) }, g { my_data->w[0] * dst.g(x, y) }, b { my_data->w[0] * dst.b(x, y) }, n { my_data->w[0] };
             auto r { my_data->w[0] * dstR[y * dstXsize + x] },
                 g { my_data->w[0] * dstG[y * dstXsize + x] },
