c44389665ccb87dd59d529aad558b9e52f371c19
Niklas Johansson <rapsabamse1@gmail.com>
2023-10-13 12:58:51 +0100

    blur

--- filters/filters/verify.sh
+++ filters/filters/verify.sh
@@ -7,6 +7,7 @@ status=0
 temp_dir=$(mktemp -d 2> /dev/null || mktemp -d -t 'mytmpdir')
 red=$(tput setaf 1)
 reset=$(tput sgr0)
+
 for thread in 1 2 4 8 16 32
 do
     for image in im1 im2 im3 im4
@@ -14,7 +15,6 @@ do
         ./blur 15 "data/$image.ppm" "$temp_dir/blur_${image}_seq.ppm"
         ./blur_par 15 "data/$image.ppm" "$temp_dir/blur_${image}_par.ppm" $thread
 
-        echo "starting"
         if ! cmp -s "$temp_dir/blur_${image}_seq.ppm" "$temp_dir/blur_${image}_par.ppm"
         then
             echo "${red}Incongruent output data detected when blurring image $image.ppm with $thread thread(s)${reset}"
