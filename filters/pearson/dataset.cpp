30d1ebba5c08aae35c0617ba140a82e62ab05dba
Niklas Johansson <rapsabamse1@gmail.com>
2023-10-14 13:09:39 +0100

    pearson

--- filters/pearson/dataset.cpp
+++ filters/pearson/dataset.cpp
@@ -8,6 +8,10 @@ Author: David Holmqvist <daae19@student.bth.se>
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <iterator>
+#include <algorithm>
+#include <iomanip>
+#include <limits>
 
 namespace Dataset {
 std::vector<Vector> read(std::string filename)
