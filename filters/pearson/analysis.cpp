33d3b10dad842088974392bba93ed0285b2e5e7b
Niklas Johansson <rapsabamse1@gmail.com>
2023-10-14 18:00:35 +0100

    pearson

--- filters/pearson/analysis.cpp
+++ filters/pearson/analysis.cpp
@@ -43,7 +43,8 @@ void* correlation_coefficients_par(void* thread_args)
     for (int sample1 { start_index }; sample1 < end_index; sample1 ++) {
         for (int sample2 = sample1 + 1; sample2 < my_data->datasets->size(); sample2++) {
             double corr = pearson((*my_data->datasets)[sample1], (*my_data->datasets)[sample2]);
-            my_data->result->at(result_i) = corr;
+            //my_data->result->at(result_i) = corr;
+            parResults.push_back(corr);
             result_i++;
             b++;
         }
@@ -52,6 +53,12 @@ void* correlation_coefficients_par(void* thread_args)
     std::cout <<"Thread: " << my_data->thread_id << " last write: " << result_i << "\n";
     std::cout <<"Thread: " << my_data->thread_id << " Inner loop: " << b << "\n";
     std::cout <<"Thread: " << my_data->thread_id << " Outer loop: " << a << "\n";
+
+    pthread_mutex_lock(&lock);
+
+    my_data->result->insert(end(a), begin(parResults), end(parResults));
+
+    pthread_mutex_unlock(&lock);
 
     /*for (auto sample1 { 0 }; sample1 < datasets.size() - 1; sample1++) {
         for (auto sample2 { sample1 + 1 }; sample2 < datasets.size(); sample2++) {
