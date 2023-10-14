203cbdc85c58b8053aef6272299c20fd7e7a2213
Niklas Johansson <rapsabamse1@gmail.com>
2023-10-14 17:34:58 +0100

    pearson

--- filters/pearson/analysis.cpp
+++ filters/pearson/analysis.cpp
@@ -81,12 +81,12 @@ std::vector<double> correlation_coefficients(std::vector<Vector> datasets, int M
         thread_data_array[i].number_of_threads = MAX_THREADS;
         thread_data_array[i].result_index = &result_index;
 
-        pthread_create(&p_threads[i], NULL, correlation_coefficients_par, (void*) &thread_data_array[i]);
+        //pthread_create(&p_threads[i], NULL, correlation_coefficients_par, (void*) &thread_data_array[i]);
     }
 
-    for (int i = 0; i < MAX_THREADS; i++) {
+    /*for (int i = 0; i < MAX_THREADS; i++) {
         pthread_join(p_threads[i], NULL);
-    }
+    }*/
     int b = 0;
     int a = 0;
     for (auto sample1 { 0 }; sample1 < datasets.size() - 1; sample1++) {
@@ -97,11 +97,11 @@ std::vector<double> correlation_coefficients(std::vector<Vector> datasets, int M
         }
         b++;
     }
-    std::cout << "Datasetsize: " << datasets.size() << "\n";
+    /*std::cout << "Datasetsize: " << datasets.size() << "\n";
     std::cout << "Vector (NEW) size: " << result.size() << "\n";
     std::cout << "Vector (OLD) size: " << old_result.size() << "\n";
     std::cout <<" Inner loop: " << a << "\n";
-    std::cout <<"Outer loop: " << b << "\n";
+    std::cout <<"Outer loop: " << b << "\n";*/
     /*for(int i = 0; i < 32; i++){
         std::cout << old_result.at(i) << " ";
     }
