4a281f1b7ef9f3c0d38bbca87ac978eb054b3cc9
Niklas Johansson <rapsabamse1@gmail.com>
2023-10-14 16:26:21 +0100

    pearson

--- filters/pearson/analysis.cpp
+++ filters/pearson/analysis.cpp
@@ -38,7 +38,7 @@ void* correlation_coefficients_par(void* thread_args)
     int a = 0;
     int result_i = start_index;
     for (int sample1 { start_index }; sample1 < end_index; sample1 ++) {
-        for (int sample2 = sample1 + 1; sample2 < end_index; sample2++) {
+        for (int sample2 = sample1 + 1; sample2 < my_data->datasets->size(); sample2++) {
             double corr = pearson((*my_data->datasets)[sample1], (*my_data->datasets)[sample2]);
             //parResults.push_back(corr);
             //parResults.insert(std::begin(*my_data->result) + (*my_data->result_index), corr);
