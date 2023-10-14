f45832f90e680fdc1c23e45dcaa23fdff94d5c32
Niklas Johansson <rapsabamse1@gmail.com>
2023-10-14 16:02:36 +0100

    pearson

--- filters/pearson/analysis.cpp
+++ filters/pearson/analysis.cpp
@@ -63,7 +63,7 @@ void* correlation_coefficients_par(void* thread_args)
 std::vector<double> correlation_coefficients(std::vector<Vector> datasets, int MAX_THREADS)
 {
     auto vector_size = ((datasets.size() * datasets.size()) / 2) + (datasets.size() / 2);
-    std::vector<double> result(vector_size);
+    std::vector<double> result {};
 
     unsigned int result_index = 0;
 
