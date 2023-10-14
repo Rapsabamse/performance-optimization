bba90bcebfe4a67c870e0a30e4cd439303d53a21
Niklas Johansson <rapsabamse1@gmail.com>
2023-10-14 16:52:40 +0100

    pearson

--- filters/pearson/analysis.cpp
+++ filters/pearson/analysis.cpp
@@ -48,8 +48,10 @@ void* correlation_coefficients_par(void* thread_args)
             result_i++;
             b++;
         }
+        a++;
     }
-    std::cout <<"Thread: " << my_data->thread_id << " Loops: " << b << "\n";
+    std::cout <<"Thread: " << my_data->thread_id << " Inner loop: " << b << "\n";
+    std::cout <<"Thread: " << my_data->thread_id << " Outer loop: " << a << "\n";
 
     /*(*pthread_mutex_lock(&lock); // prevent race conditions between threads writing to result
     std::cout << "Thread " << my_data->thread_id << ":s loops:" << a << "\n";
@@ -90,7 +92,7 @@ std::vector<double> correlation_coefficients(std::vector<Vector> datasets, int M
     for (int i = 0; i < MAX_THREADS; i++) {
         pthread_join(p_threads[i], NULL);
     }
-
+    int b = 0;
     int a = 0;
     for (auto sample1 { 0 }; sample1 < datasets.size() - 1; sample1++) {
         for (auto sample2 { sample1 + 1 }; sample2 < datasets.size(); sample2++) {
@@ -98,11 +100,13 @@ std::vector<double> correlation_coefficients(std::vector<Vector> datasets, int M
             old_result.push_back(corr);
             a++;
         }
+        b++;
     }
     std::cout << "Datasetsize: " << datasets.size() << "\n";
     std::cout << "Vector (NEW) size: " << result.size() << "\n";
     std::cout << "Vector (OLD) size: " << old_result.size() << "\n";
-    std::cout << "Real loops: " << a << "\n";
+    std::cout <<"Thread: " << my_data->thread_id << " Inner loop: " << a << "\n";
+    std::cout <<"Thread: " << my_data->thread_id << " Outer loop: " << b << "\n";
     /*for(int i = 0; i < 32; i++){
         std::cout << old_result.at(i) << " ";
     }
